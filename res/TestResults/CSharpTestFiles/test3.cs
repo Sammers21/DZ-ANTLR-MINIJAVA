a489677be6eecaf103b1cdecf9a7facbd8a80232
Sammers21 <titantins@ya.ru>
2016-07-03 19:08:06 +0300

    bin commit

---
+++ res/TestResults/CSharpTestFiles/test3.cs
@@ -0,0 +1,9 @@
+﻿using System;
+class A {
+public static void Main(string[] args){
+double x=2.0;
+double z= x * 2.5;
+Console.WriteLine(z);
+
+}
+}
---
+++ res/TestResults/CSharpTestFiles/test4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class A {
+public static void onemult(double numb,double pow){
+numb=pow*numb;
+Console.WriteLine("one mult is:");
+Console.WriteLine(numb);
+
+}
+public static void twomult(double number,double POW){
+number=POW*number;
+number=POW*number;
+Console.WriteLine("two mult is:");
+Console.WriteLine(number);
+
+}
+public static void Main(string[] args){
+double x=2.0;
+double[] X={1.0,2.0,4.0,8.0,16.0,32.0,64.0,128.0,256.0};
+foreach (double d in X){
+onemult(x,d);
+twomult(x,d);
+
+}
+
+}
+}
